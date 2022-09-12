d9187b7097fff7ab7a7eecd07e590d75f5236eb8
ilasw <pagliaroluca@gmail.com>
2022-09-12 11:38:34 +0200

    feat: added link to catalog page, now refresh on click

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import App from './App';
+import { Home, Catalog } from './pages';
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
 root.render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/catalogo" element={<Catalog />} />
+        <Route path="*" element={<div>Pagina non trovata</div>} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>
 );
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
 
 export const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <h1>Home</h1>
+      <a
+        href="/catalogo"
+        title="Vedi tutte le ricette"
+        ariaLabel="Vedi tutte le ricette"
+      >
+        Vai al catalogo
+      </a>
+    </div>
+  );
 };
 
 export default Home;
